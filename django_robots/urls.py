c75deb7afae45cc69a706b62e1903e05f5989161
Joseph Bergantine <jbergantine@gmail.com>
2013-05-05 14:01:15 -0600

    text/plain mimetype

--- django_robots/urls.py
+++ django_robots/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('django.views.generic.simple',
     url(r'^$', 'direct_to_template', 
-        {'template': 'robots/robots.txt', 'mimetype': 'text/html'}, 'robots'),
+        {'template': 'robots/robots.txt', 'mimetype': 'text/plain'}, 'robots'),
 )
