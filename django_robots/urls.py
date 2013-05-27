61694f558a947dc5a7bd1b4cfd2f4bdad1a46198
Joseph Bergantine <jbergantine@gmail.com>
2013-05-27 00:03:11 -0500

    Updated to Class-based views syntax.

--- django_robots/urls.py
+++ django_robots/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
-
-urlpatterns = patterns('django.views.generic.simple',
-    url(r'^$', 'direct_to_template', 
-        {'template': 'robots/robots.txt', 'mimetype': 'text/plain'}, 'robots'),
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='robots/robots.txt', content_type='text/plain'), name='robots'),
 )
