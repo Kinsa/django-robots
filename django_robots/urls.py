a008aa45c4b6c3459f4961e616f8130145833183
Joe Bergantine <jbergantine@gmail.com>
2015-12-02 07:54:53 -0700

    remove use of django.conf.url.patterns in django > 1.8
    Resolve
    
    ```
    /home/vagrant/.virtualenvs/djangoproj/src/django-robots/django_robots/urls.py:5: RemovedInDjango110Warning: django.conf.urls.patterns() is deprecated and will be removed in Django 1.10. Update your urlpatterns to be a list of django.conf.urls.url() instances instead.
      url(r'^$', TemplateView.as_view(template_name='robots/robots.txt', content_type='text/plain'), name='robots'),
    ```

--- django_robots/urls.py
+++ django_robots/urls.py
@@ -1,6 +1,19 @@
-from django.conf.urls import patterns, url
+import django
+
+if django.VERSION < (1, 9):
+    from django.conf.urls import patterns
+
+from django.conf.urls import url
+
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
+urls = [
     url(r'^$', TemplateView.as_view(template_name='robots/robots.txt', content_type='text/plain'), name='robots'),
-)
+]
+
+if django.VERSION < (1, 9):
+    urlpatterns = patterns('',
+        [url for url in urls]
+    )
+else:
+    urlpatterns = urls
