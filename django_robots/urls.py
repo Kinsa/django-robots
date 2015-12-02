a73d0e8ca399b149aa4e09357e8c467c68dee7bd
Joe Bergantine <jbergantine@gmail.com>
2015-12-02 15:50:15 -0700

    not very dry solution to django 1.9 && < 1.9 split

--- django_robots/urls.py
+++ django_robots/urls.py
@@ -1,19 +1,32 @@
 import django
 
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
 if django.VERSION < (1, 9):
     from django.conf.urls import patterns
 
-from django.conf.urls import url
 
-from django.views.generic import TemplateView
-
-urls = [
-    url(r'^$', TemplateView.as_view(template_name='robots/robots.txt', content_type='text/plain'), name='robots'),
+urlpatterns = [
+    url(
+        r'^$',
+        TemplateView.as_view(
+            template_name='robots/robots.txt',
+            content_type='text/plain'
+        ),
+        name='robots'
+    ),
 ]
 
 if django.VERSION < (1, 9):
-    urlpatterns = patterns('',
-        [url_i for url_i in urls]
+    urlpatterns = patterns(
+        '',
+        url(
+            r'^$',
+            TemplateView.as_view(
+                template_name='robots/robots.txt',
+                content_type='text/plain'
+            ),
+            name='robots'
+        ),
     )
-else:
-    urlpatterns = urls
--- tests/urls.py
+++ tests/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^robots.txt$', include('django_robots.urls')),
+    url(r'^robots.txt', include('django_robots.urls')),
 ]
