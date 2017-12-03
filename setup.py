44c2a905b36cf99911e8ffb7de2a976a9e81b5d2
Joe Bergantine <jbergantine@gmail.com>
2017-12-03 09:58:00 -0700

    Django 2.0 updates
    - Reconfigured setup file
    - Dropped Django 1.7 support
    - Reconfigured tox and travis config files

--- setup.py
+++ setup.py
@@ -1,25 +1,39 @@
+import os
+
 from setuptools import setup, find_packages
 
+description = "robots.txt File Generation Django"
+long_description = description
+
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
 
 setup(
     name='django-robots',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(exclude=["tests"]),
-    install_requires=['Django>=1.7'],
+    install_requires=[
+        'Pillow>=3.1.1',
+        'sorl-thumbnail>=3.2.5',
+        'Django>=1.8'
+    ],
     author='Joe Bergantine',
     author_email='jbergantine@gmail.com',
-    description="robots.txt File Generation Django",
+    description=description,
+    long_description=long_description,
     url='https://github.com/jbergantine/django-robots',
-    download_url='https://github.com/jbergantine/django-robots/tarball/1.1.1',
+    download_url='https://github.com/jbergantine/django-robots/tarball/1.1.2',
     license='New BSD License',
     platforms=['any'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
@@ -31,7 +45,10 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Topic :: Utilities',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     test_suite="runtests.runtests",
     include_package_data=True,
